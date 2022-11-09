84eecb000b02735c8e25755ac03931593a9718ed
Hammuda007 <sheakho19@gmail.com>
2022-11-09 03:19:33 +0100

    add variabel task

---
+++ 01-Variabels/tasks.py
@@ -0,0 +1,77 @@
+# 1: Create a Boolean variable named x
+
+x= True
+
+# 2 : Create an integer variabel named y
+
+y= 5
+
+# 3 : Create a float variabel named z
+
+z= 2.7
+
+# 4  : Create a string variabel named s
+
+s= 'Muhammed'
+
+# 5 : Convert a int variabel to float
+
+x= 7
+print(float(x))
+ 
+
+
+# 6 : Can we convert the str to int?
+
+x= '19'
+print(int(x))
+
+# 7 : Create a list of numbers from 1 to 5
+
+x=[1,2,3,4,5]
+
+# 8 : Create a Tuple from 10 to 15
+
+y= (10,11,12,13,14,15)
+
+
+ # 9 : Convert the list to tuple
+
+x=[1,2,3,4,5]
+y_tuple= tuple(x)
+print(y_tuple)
+
+# 10 : Create a dic of 3 values
+
+z={1: 'Muhammed', 2: 'Mahmoud', 3: 'Ali'}
+
+
+# 11 : Can we use semi colon ; with Python ?
+
+Python does not  use Semicolons but it is not restricted
+
+# 12 : Python is  interpreted or compiled ?
+
+interpreted
+
+# 13 : What is the different between low level and high level ?
+
+low level: The language of the computer
+It consists of 1 , 0
+it is easy to be Executed
+It is hard to program
+Can run only of one kind of computer and have to be rewritten to another
+Like C , Assembly
+
+high level: Easy to program
+Takes less time to be coded
+Easy to be corrected
+Portable (only one program can run on any pc)
+Like java , c# & python
+
+
+ 
+
+ 
+ 
+
---
+++ 02-Operators/tasks.py
@@ -0,0 +1,54 @@
+# 1 : what is the differences between = , == ?
+'''
+ = mean the variabel has this value , e.g x= 5
+
+ == mean question if this variabel has this value
+'''
+ # 2 : what do we mean by using =!
+'''
+ check if the value of two operands is not equal , if not then it is True
+'''
+
+# 3 : what is the operator precedence ?
+'''
+(),**, /or//,*, +,-
+'''
+
+# 4 : Create a variabel x with value of 10
+
+x = 10
+
+# 5 : Increse x value by 15 using assignment operatores
+x += 5
+
+# 6 : Divide the x value by 5 using assignment operatores
+
+x /= 5
+
+# 7 : Multiply the x value by 10 using assignment operatores
+
+x *= 10
+
+# 8 : Get Module of x on 3 using //////////
+
+30%3
+
+# 9 : Using python print module 11 to 4
+
+print(11%4)
+
+# 10 : print the exponent of 2,3
+x = 2
+x **=3
+print (x)
+
+# 11 : Divide 11 by 3 using python division
+
+x=11
+x/=3
+print(x)
+
+# 12 : Can we divid 11 by 3 and get an int number
+
+yes with //=  
+
