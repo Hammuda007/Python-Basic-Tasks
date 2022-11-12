d09dd92c2478cb56065506b131421fad8cc7664b
Hammuda007 <sheakho19@gmail.com>
2022-11-12 11:00:45 +0100

    add conditions tasks

---
+++ 04-Conditions/tasks.py
@@ -0,0 +1,159 @@
+# 1: Check if 10 is bigger than 15 or not
+'''
+x= 10
+if x > 15 :
+    print ('x bigger than 15')
+
+else:
+    print('error')
+'''
+
+# 2: If 10 is not bigger than 15 print x is smaller than 15
+'''
+x= 10
+if x > 15 :
+    print ('x bigger than 15')
+
+else:
+    print('x smaller than 15')
+'''
+
+# 3 : In wich cases we will use all
+'''
+We use all when we have a lot of and conditions ->> all = a many of and
+
+'''
+
+# 4 : What is the differences between all, and
+'''
+all : they are used to shorten codes, as we do not to repeat and on each line
+'''
+
+# 5 : What is the different between any and or
+'''
+any used when we have a lot of or conditions , as we do not repeat or in each line and we just use any to have a short code 
+'''
+
+# 6 : If we need all the conditionsto be true we will use ......
+'''
+we use and or all 
+'''
+
+# 7 : What is the different between  if, elif
+'''
+When we have more as 1 condition we use after first condition if we use elif and else will be exceuted 
+'''
+
+# 8 ; What is the different between  elif, else
+'''
+elif use when we have more one conditins but else use if the condition is not met  '''
+
+# 9 : Can we use more then one elif
+'''
+yes,we can
+'''
+
+# 10 : Can we use more than one else
+'''
+no, we can not , because else uses if the condition flase is
+'''
+
+# 11 : Write s simple ternary operator
+'''
+x = 17
+if x> 20 :
+    print (' x is not bigger than 20')
+elif x==17:
+    print (' x is a true value ')
+
+else :
+    print('none')
+'''
+
+# 12 : In elif , python will check all the conditions no matter what
+'''
+yes
+'''
+
+# 13 : In elif we use else for
+'''
+When the condition met the programm go on , when not then print else 
+'''
+
+# 14 : If we have this List [2,4,6,8,10]
+
+# 1- check if 4 in this list or not
+'''
+x= [2,4,6,8,10]
+if 4 in x :
+    print(' 4 in the list ')
+else :
+    print('4 is not in the list')
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+# 2- check if 4 and 6 in this list or not
+'''
+x= [2,4,6,8,10]
+if 4 in x and 6 in x:
+    print(' 4,6 in the list ')
+
+else :
+    print('4,6 is not in the list')
+'''
+
+
+
+
+
+
+
+
+# 3- check if 3 and 6 in this list
+'''
+x= [2,4,6,8,10]
+if 3 in x or 6 in x :
+    print(' true')
+else :
+    print('false')
+'''
+
+'''
+x= [2,4,6,8,10]
+if 3 in x and 6 in x :
+    print(' true')
+else :
+    print('3 not in the list ')
+'''
+# 4- check if 2 ,4 and 5 in this list
+
+x= [2,4,6,8,10]
+if all([x==2, x==4, x==5]):
+    print ('all parameters in the list')
+
+
+
+else :
+    print('all parameters is not  in the list')
+
+
+
+
+
+
+
+
+
+
+
+
